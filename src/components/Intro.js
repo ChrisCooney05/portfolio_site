f6cc73f18ace5531f8fc9e9c6da90e00e3eb2982
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-11 13:58:41 +0100

    added intro component, need to style

---
+++ src/components/Intro.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Intro extends Component {
+  render() { 
+    return (  
+      <div>
+        Hi There, I'm Chris<br/>
+        A Full-Stack Developer
+      </div>
+    );
+  }
+}
+
+export default Intro;
