308d3129a668f74160c96c92570e67d9f2aacd4b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-12 13:33:16 +0100

    added a new project and each link opens in a new window

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ class Project extends Component {
   render() {
     const { title, description, link } = this.props
     return (
-      <a href={link} className='tc dib br3 pa5 ma2 grow bw2 shadow-5 pa2 w-30 mr2 no-underline black'>
+      <a href={link} className='tc dib br3 pa5 ma2 grow bw2 shadow-5 pa2 w-30 mr2 no-underline black' target='_blank'>
         <span className='f2 b green'>{title}</span>
         <p>{description}</p>
       </a>
