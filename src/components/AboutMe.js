b83efd8d7f78835254f9eaacad3bf941c6fb5550
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-12 13:55:32 +0100

    fixing issue with netlify deploy failure

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,15 +8,15 @@ class AboutMe extends Component {
         <p className='f2 b'>About Me</p>
         <div className='tl b dib pa3 f5 '>
           Fun fact, after years of wanting to learn how to code, but telling <br />
-          myself "programming is magic and there is no way you could understand it" <br />
+          myself "programming is magic and there is no way you could understand it". <br />
           I decided, I fancy learning me some of this magic, loved it, quit <br />
           my job of 5 years, studied software development and here I am fresh of the press <br />
           and making myself this shinny website!<br />
           Two things are nearly always on my mind. What shall I learn next? <br />
-          and what shall I have for dinner? one less important than the other, but both <br />
+          And what shall I have for dinner? One less important than the other, but both <br />
           important to understand how my mind works. <br />
-          a bit further down you can see a few of my projects, but you can also visit my github<br />
-          or get in contact with me via email or linkedIn<br /><br />
+          A bit further down you can see a few of my projects, but you can also visit my github<br />
+          or get in contact with me via email or linkedIn.<br /><br />
           <div className='flex'>
             <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black" href="https://github.com/ChrisCooney05">GitHub</a>
             <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black" href="https://www.linkedin.com/in/chris-cooney-003028160/">LinkedIn</a>
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,10 @@ class Project extends Component {
   render() {
     const { title, description, link } = this.props
     return (
-      <a href={link} className='tc dib br3 pa5 ma2 grow bw2 shadow-5 pa2 w-30 mr2 no-underline black' target='_blank'>
+      <a href={link}
+        className='tc dib br3 pa5 ma2 grow bw2 shadow-5 pa2 w-30 mr2 no-underline black'
+        target='_blank'
+        rel="noopener noreferrer">
         <span className='f2 b green'>{title}</span>
         <p>{description}</p>
       </a>
