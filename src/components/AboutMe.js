f35ddfd634d8c48eac0262c69cba2ced457da9da
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-12 14:00:16 +0100

    all contact buttons will now open in a new tab rather than over my portfolio

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -18,9 +18,18 @@ class AboutMe extends Component {
           A bit further down you can see a few of my projects, but you can also visit my github<br />
           or get in contact with me via email or linkedIn.<br /><br />
           <div className='flex'>
-            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black" href="https://github.com/ChrisCooney05">GitHub</a>
-            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black" href="https://www.linkedin.com/in/chris-cooney-003028160/">LinkedIn</a>
-            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black" href="mailto:chris.cooney05@hotmail.co.uk">Email Me</a>
+            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+              href="https://github.com/ChrisCooney05"
+              target='_blank'
+              rel="noopener noreferrer">GitHub</a>
+            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+              href="https://www.linkedin.com/in/chris-cooney-003028160/"
+              target='_blank'
+              rel="noopener noreferrer">LinkedIn</a>
+            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+              href="mailto:chris.cooney05@hotmail.co.uk"
+              target='_blank'
+              rel="noopener noreferrer">Email Me</a>
           </div>
         </div>
         <img src={image} alt='myImage' width='200px' className='bw2 shadow-5 br3' />
