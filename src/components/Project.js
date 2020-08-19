0ee070b8f8257f52f437bddc208e4a88cbf61ffd
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-19 17:25:43 +0100

    fixed issue with project centering

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
 
 class Project extends Component {
   render() {
-    const { title, description, link } = this.props
+    const { title, description, link } = this.props;
     return (
-      <a href={link}
-        className='tc dib br3 pa5 ma2 grow bw2 shadow-5 pa2 w-30 mr2 no-underline black'
-        target='_blank'
-        rel="noopener noreferrer">
-        <span className='f2 b green'>{title}</span>
+      <a
+        href={link}
+        className="tc dib br3 pa5 ma2 grow bw2 shadow-5 pa2 w-30 mr2 no-underline black"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="f2 b green">{title}</span>
         <p>{description}</p>
       </a>
     );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react'
-import Project from './Project'
-import projectArray from '../projectJson'
-
+import React, { Component } from "react";
+import Project from "./Project";
+import projectArray from "../projectJson";
 
 class ProjectList extends Component {
   render() {
     const projectList = projectArray.map((project, i) => {
       return (
-        <Project key={i} title={project.title} description={project.description} link={project.link} />
+        <Project
+          key={i}
+          title={project.title}
+          description={project.description}
+          link={project.link}
+        />
       );
-    })
+    });
     return (
-      <div className='animate__animated animate__fadeIn animate__delay-4s'>
-        <p className='f2 b tc'>Projects</p>
-        <div className='tc flex flex-wrap'>
-          {projectList}
-        </div>
+      <div className="animate__animated animate__fadeIn animate__delay-4s tc">
+        <p className="f2 b tc">Projects</p>
+        <div className="flex flex-wrap justify-center">{projectList}</div>
       </div>
-    )
+    );
   }
 }
 
-export default ProjectList
+export default ProjectList;
