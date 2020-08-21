39b9fe7ebb919b478394044bda1b7f550be86970
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-08-21 13:55:23 +0100

    Able to get background image to load via json

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
+import styled from "@emotion/styled/macro";
 
 class Project extends Component {
   render() {
-    const { title, description, link } = this.props;
+    const { title, description, link, img } = this.props;
+    console.log(img);
+    const Background = styled.div({
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      color: "#FFF",
+      position: "relative",
+      width: "500px",
+      height: "350px",
+      cursor: "pointer",
+      backgroundImage: `url(${img})`,
+    });
     return (
-      <a
-        href={link}
-        className="tc dib br3 pa5 ma2 grow bw2 shadow-5 pa2 w-30 mr2 no-underline black"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <span className="f2 b green">{title}</span>
-        <p>{description}</p>
-      </a>
+      <Background>
+        <a
+          href={link}
+          className="no-underline black"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="f2 b green">{title}</span>
+          <p>{description}</p>
+        </a>
+      </Background>
     );
   }
 }
