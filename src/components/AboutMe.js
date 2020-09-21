9c8ec10d958823c3a3b49526e6b1d811fee3af00
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-21 14:54:31 +0100

    Changed Aboutme section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,40 +1,69 @@
-import React, { Component } from 'react'
-import image from '../assets/me.png'
+import React, { Component } from "react";
+import image from "../assets/me.png";
 
 class AboutMe extends Component {
   render() {
     return (
-      <div className='tc animate__animated animate__fadeIn animate__delay-3s'>
-        <p className='f2 b'>About Me</p>
-        <div className='tl b dib pa3 f5 '>
-          Fun fact, after years of wanting to learn how to code, but telling <br />
-          myself "programming is magic and there is no way you could understand it". <br />
-          I decided, I fancy learning me some of this magic, loved it, quit <br />
-          my job of 5 years, studied software development and here I am fresh of the press <br />
-          and making myself this shinny website!<br />
-          Two things are nearly always on my mind. What shall I learn next? <br />
-          And what shall I have for dinner? One less important than the other, but both <br />
-          important to understand how my mind works. <br />
-          A bit further down you can see a few of my projects, but you can also visit my github<br />
-          or get in contact with me via email or linkedIn.<br /><br />
-          <div className='flex'>
-            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+      <div className="tc animate__animated animate__fadeIn animate__delay-3s">
+        <h3 className="f2 b">About Me</h3>
+        <p className="tl dib pa3 f5 fw6">
+          Thanks for popping in and checking out my portfolio!
+          <br />
+          <br />
+          Before making this site I was a Key account manager with a packaging
+          company, before that <br />
+          I have had many a job, from retail management to personal trainer.
+          I've never really <br />
+          known what I wanted to do until one day on the train I started playing
+          with a 'Learn to code' <br />
+          app. This sparked something in me, so much so that I decided to make a
+          career of it! I took part in a 12 week <br />
+          coding bootcamp with a company called makers and now here I am, a full
+          stack dev with a shiny website.
+          <br />
+          A bit further down you can see a few of my projects, but you can also
+          visit my github to checkout my code!
+          <br />
+          <br />
+          I would love to hear from you, so drop me a message via email or
+          linkedIn.
+          <br />
+          <br />
+          <div className="flex">
+            <a
+              className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
               href="https://github.com/ChrisCooney05"
-              target='_blank'
-              rel="noopener noreferrer">GitHub</a>
-            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
               href="https://www.linkedin.com/in/chris-cooney-003028160/"
-              target='_blank'
-              rel="noopener noreferrer">LinkedIn</a>
-            <a className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LinkedIn
+            </a>
+            <a
+              className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
               href="mailto:chris.cooney05@hotmail.co.uk"
-              target='_blank'
-              rel="noopener noreferrer">Email Me</a>
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Email Me
+            </a>
           </div>
-        </div>
-        <img src={image} alt='myImage' width='200px' className='bw2 shadow-5 br3' />
+        </p>
+        <img
+          src={image}
+          alt="myImage"
+          width="200px"
+          className="bw2 shadow-5 br3"
+        />
       </div>
-    )
+    );
   }
 }
 
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,15 +4,15 @@ class Intro extends Component {
   render() {
     return (
       <div className="mh5-ns">
-        <h1 className="animate__animated animate__fadeIn green f-headline ma2 b">
+        <h1 className="animate__animated animate__fadeIn green f-headline ma1 b">
           Hi There
         </h1>
 
-        <h1 className="animate__animated animate__fadeIn animate__delay-1s green f-headline ma2 b">
+        <h1 className="animate__animated animate__fadeIn animate__delay-1s green f-headline ma1 b">
           I'm Chris
         </h1>
 
-        <h1 className="animate__animated animate__fadeIn animate__delay-2s green f-headline ma2 b">
+        <h1 className="animate__animated animate__fadeIn animate__delay-2s green f-headline ma1 b">
           A Full Stack Developer
         </h1>
       </div>
