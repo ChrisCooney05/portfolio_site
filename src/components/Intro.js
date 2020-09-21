6777e9ea3b8830ef8613dfc232a9dfd4fb56bed5
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-09-21 15:26:29 +0100

    Added flec to title for more responce

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,16 +3,16 @@ import React, { Component } from "react";
 class Intro extends Component {
   render() {
     return (
-      <div className="mh5-ns">
-        <h1 className="animate__animated animate__fadeIn green f-headline ma1 b">
+      <div className="mh5-ns flex flex-column">
+        <h1 className="animate__animated animate__fadeIn green f-headline ma1 b w-75">
           Hi There
         </h1>
 
-        <h1 className="animate__animated animate__fadeIn animate__delay-1s green f-headline ma1 b">
+        <h1 className="animate__animated animate__fadeIn animate__delay-1s green f-headline ma1 b w-75">
           I'm Chris
         </h1>
 
-        <h1 className="animate__animated animate__fadeIn animate__delay-2s green f-headline ma1 b">
+        <h1 className="animate__animated animate__fadeIn animate__delay-2s green f-headline ma1 b w-75">
           A Full Stack Developer
         </h1>
       </div>
