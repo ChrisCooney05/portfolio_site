a142307f12e9216375c29d10a3ec99014a134861
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-14 15:13:30 +0000

    Converted ProjectList into a func

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import Project from "./Project";
 import { projectArray } from "../projectJson";
 
-class ProjectList extends Component {
-  render() {
-    const projectList = projectArray.map((project, i) => {
-      return (
-        <Project
-          key={i}
-          title={project.title}
-          description={project.description}
-          link={project.link}
-          img={project.img}
-        />
-      );
-    });
+const ProjectList = () => {
+  const projectList = projectArray.map((project, i) => {
     return (
-      <div className="animate__animated animate__fadeIn animate__delay-4s tc">
-        <p className="f2 b tc">Projects</p>
-        <div className="flex flex-wrap justify-center">{projectList}</div>
-      </div>
+      <Project
+        key={i}
+        title={project.title}
+        description={project.description}
+        link={project.link}
+        img={project.img}
+      />
     );
-  }
-}
+  });
+  return (
+    <div className="animate__animated animate__fadeIn animate__delay-4s tc">
+      <p className="f2 b tc">Projects</p>
+      <div className="flex flex-wrap justify-center">{projectList}</div>
+    </div>
+  );
+};
 
 export default ProjectList;
