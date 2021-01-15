6e058a4bea170e92d7ea1c3209594113e2c78e07
Chris Cooney <chris.cooney05@hotmail.co.uk>
2021-01-15 09:14:44 +0000

    Converted all components into functions

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import image from "../assets/me.png";
 
-class AboutMe extends Component {
-  render() {
-    return (
-      <div className="tc animate__animated animate__fadeIn animate__delay-3s">
-        <h3 className="f2 b">About Me</h3>
-        <p className="tl dib pa3 f5 fw6">
-          Thanks for popping in and checking out my portfolio!
-          <br />
-          <br />
-          Before making this site I was a Key account manager with a packaging
-          company, before that <br />
-          I have had many a job, from retail management to personal trainer.
-          I've never really <br />
-          known what I wanted to do until one day on the train I started playing
-          with a 'Learn to code' <br />
-          app. This sparked something in me, so much so that I decided to make a
-          career of it! I took part in a 12 week <br />
-          coding bootcamp with a company called makers and now here I am, a full
-          stack dev with a shiny website.
-          <br />
-          A bit further down you can see a few of my projects, but you can also
-          visit my github to checkout my code!
-          <br />
-          <br />
-          I would love to hear from you, so drop me a message via email or
-          linkedIn.
-          <br />
-          <br />
-          <div className="flex">
-            <a
-              className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
-              href="https://github.com/ChrisCooney05"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-            <a
-              className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
-              href="https://www.linkedin.com/in/chris-cooney-003028160/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>
-            <a
-              className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
-              href="mailto:chris.cooney05@hotmail.co.uk"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Email Me
-            </a>
-          </div>
-        </p>
-        <img
-          src={image}
-          alt="myImage"
-          width="200px"
-          className="bw2 shadow-5 br3"
-        />
-      </div>
-    );
-  }
-}
+const AboutMe = () => {
+  return (
+    <div className="tc animate__animated animate__fadeIn animate__delay-3s">
+      <h3 className="f2 b">About Me</h3>
+      <p className="tl dib pa3 f5 fw6">
+        Thanks for popping in and checking out my portfolio!
+        <br />
+        <br />
+        Before making this site I was a Key account manager with a packaging
+        company, before that <br />
+        I have had many a job, from retail management to personal trainer. I've
+        never really <br />
+        known what I wanted to do until one day on the train I started playing
+        with a 'Learn to code' <br />
+        app. This sparked something in me, so much so that I decided to make a
+        career of it! I took part in a 12 week <br />
+        coding bootcamp with a company called makers and now here I am, a full
+        stack dev with a shiny website.
+        <br />
+        A bit further down you can see a few of my projects, but you can also
+        visit my github to checkout my code!
+        <br />
+        <br />
+        I would love to hear from you, so drop me a message via email or
+        linkedIn.
+        <br />
+        <br />
+        <div className="flex">
+          <a
+            className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+            href="https://github.com/ChrisCooney05"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+            href="https://www.linkedin.com/in/chris-cooney-003028160/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="w-25 pa3 mr2 f6 link dim br2 ba bw1 ph3 pv2 mb2 dib black"
+            href="mailto:chris.cooney05@hotmail.co.uk"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Email Me
+          </a>
+        </div>
+      </p>
+      <img
+        src={image}
+        alt="myImage"
+        width="200px"
+        className="bw2 shadow-5 br3"
+      />
+    </div>
+  );
+};
 
 export default AboutMe;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,23 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Intro extends Component {
-  render() {
-    return (
-      <div className="mh5-ns flex flex-column">
-        <h1 className="animate__animated animate__fadeIn green f-headline ma1 b w-75">
-          Hi There
-        </h1>
+const Intro = () => {
+  return (
+    <div className="mh5-ns flex flex-column">
+      <h1 className="animate__animated animate__fadeIn green f-headline ma1 b w-75">
+        Hi There
+      </h1>
 
-        <h1 className="animate__animated animate__fadeIn animate__delay-1s green f-headline ma1 b w-75">
-          I'm Chris
-        </h1>
+      <h1 className="animate__animated animate__fadeIn animate__delay-1s green f-headline ma1 b w-75">
+        I'm Chris
+      </h1>
 
-        <h1 className="animate__animated animate__fadeIn animate__delay-2s green f-headline ma1 b w-75">
-          A Full Stack Developer
-        </h1>
-      </div>
-    );
-  }
-}
+      <h1 className="animate__animated animate__fadeIn animate__delay-2s green f-headline ma1 b w-75">
+        A Full Stack Developer
+      </h1>
+    </div>
+  );
+};
 
 export default Intro;
